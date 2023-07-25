b6e779a27154142b256c3a63dfa93609a5372819
Ryan Daniels <ryandaniels230@gmail.com>
2023-07-25 12:33:49 -0400

    fixed coloring and font size issues on widget and dropdowns, changed validation error color

--- src/js/app.js
+++ src/js/app.js
@@ -23,8 +23,11 @@
     }
  */
 
-function checkValidText(text) {
-  if (text === null || text.length <= 0) {
+function checkValidName(text) {
+
+  let regName = /^(?=.{1,50}$)[a-zA-Z]+(?: [a-zA-Z]+)*$/;
+
+  if (text === null || text.length <= 0 || !regName.test(text)) {
     return false;
   }
 
@@ -32,59 +35,82 @@ function checkValidText(text) {
 }
 
 function checkFirstName(firstName, errorElement) {
+
+  let firstNameElement = document.getElementById(errorElement);
+
   //check first name
-  if (checkValidText(firstName)) {
-    document.getElementById(errorElement).innerHTML = null;
+  if (checkValidName(firstName)) {
+    firstNameElement.classList.add("is-valid");
+    firstNameElement.classList.remove("is-invalid");
     return firstName;
   }
 
   else {
-    document.getElementById(errorElement).innerHTML = "Please enter a valid first name"
-    return "First Name Here";
+    firstNameElement.classList.add("is-invalid");
+    firstNameElement.classList.remove("is-valid");
+    return "First Name";
   }
 }
 
 function checkLastName(lastname, errorElement) {
+
+  let lastNameElement = document.getElementById(errorElement);
+
   //check last name
-  if (checkValidText(lastname)) {
-    document.getElementById(errorElement).innerHTML = null;
+  if (checkValidName(lastname)) {
+    lastNameElement.classList.add("is-valid");
+    lastNameElement.classList.remove("is-invalid");
     return lastname;
   }
 
   else {
-    document.getElementById(errorElement).innerHTML = "Please enter a valid last name"
-    return "Last Name Here";
+    lastNameElement.classList.add("is-invalid");
+    lastNameElement.classList.remove("is-valid");
+    return "Last Name";
   }
 }
 
-function checkDropDown(dropDownValue){
-  if (dropDownValue === null){
+function checkDropDown(dropDownValue, errorElement) {
+
+  let dropdownElement = document.getElementById(errorElement);
+
+  if (dropDownValue === null || dropdownElement.selectedIndex <= 0) {
+    dropdownElement.classList.add("is-invalid");
+    dropdownElement.classList.remove("is-valid");
     return "Please select an option";
   }
 
-  else{
+  else {
+    dropdownElement.classList.add("is-valid");
+    dropdownElement.classList.remove("is-invalid");
     return dropDownValue;
   }
 }
 
-function checkURL(url, errorElement){
-  if (url === null || url.length <= 0){
-    document.getElementById(errorElement).innerHTML = null;
+function checkURL(url, errorElement) {
+
+  let regValidURL = /^(https?:\/\/)?[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,}(\/\S*)?$/;
+  let urlElement = document.getElementById(errorElement);
+
+  if (url === null || url.length <= 0 || !regValidURL.test(url)) {
+    urlElement.classList.add("is-invalid");
+    urlElement.classList.remove("is-valid");
     return "#";
   }
 
-  else{
-    document.getElementById(errorElement).innerHTML = "Please enter a valid URL"
+  else {
+    urlElement.classList.add("is-valid");
+    urlElement.classList.remove("is-invalid");
     return url;
   }
 }
 
-function checkCover(cover, background){
-  if (cover == false){
+function checkCover(cover, background) {
+  if (cover == false) {
     return "<div class='cover'></div>";
   }
 
-  else{
+  else {
     return `<div class="cover"><img src="${background}" /></div>`;
   }
 }
@@ -97,8 +123,8 @@ function renderBody(variables = {}) {
   htmlContent.push(`${checkCover(variables.includeCover, variables.background)}`);
   htmlContent.push(`<img src="${variables.avatarURL}" class="photo" />`);
   htmlContent.push(`<h1>${checkFirstName(variables.name, "error-label-name")} ${checkLastName(variables.lastname, "error-label-last-name")}</h2>`);
-  htmlContent.push(`<h2>${checkDropDown(variables.role)}</h2>`);
-  htmlContent.push(`<h3>${checkDropDown(variables.city)}, ${checkDropDown(variables.country)}</h3>`);
+  htmlContent.push(`<h2>${checkDropDown(variables.role, "role-dropdown")}</h2>`);
+  htmlContent.push(`<h3>${checkDropDown(variables.city, "city-dropdown")}, ${checkDropDown(variables.country, "country-dropdown")}</h3>`);
   htmlContent.push(`<ul class="${variables.socialMediaPosition}">`);
   htmlContent.push(`<li><a href="${checkURL(variables.twitter, "error-label-url-twitter")}" target="_blank"><i class="fab fa-twitter"></i></a></li>`);
   htmlContent.push(`<li><a href="${checkURL(variables.github, "error-label-url-github")}" target="_blank"><i class="fab fa-github"></i></a></li>`);
@@ -117,13 +143,16 @@ function render(variables = {}) {
   // here we ask the logical questions to make decisions on how to build the html
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
 
-    document.querySelector("#widget_content").innerHTML = renderBody(variables);
+  document.querySelector("#widget_content").innerHTML = renderBody(variables);
 }
 
 /**
  * Don't change any of the lines below, here is where we do the logic for the dropdowns
  */
 window.onload = function () {
+
+  checkCityCountry();
+
   window.variables = {
     // if includeCover is true the algorithm should
     includeCover: true,
@@ -150,7 +179,6 @@ window.onload = function () {
     elm.addEventListener("change", function (e) {
       // <- add a listener to every input
       const attribute = e.target.getAttribute("for"); // when any input changes, collect the value
-      console.log(attribute);
       let values = {};
       values[attribute] =
         this.value == "" || this.value == "null"
@@ -164,3 +192,72 @@ window.onload = function () {
     });
   });
 };
+
+
+//===================================================
+// City and Country Dropdown Checks
+//===================================================
+
+function checkCityCountry() {
+
+  // Get references to the two dropdowns
+  const cityDropdown = document.getElementById("city-dropdown");
+  const countryDropdown = document.getElementById("country-dropdown");
+
+  // Define the options for both dropdowns based on the selection of the other dropdown
+  const optionsBySelection = {
+    USA: ["Chicago", "Miami", "New York"],
+    Germany: ["Dortmund", "Frankfurt", "Munich"],
+    Venezuela: ["Barquisimeto", "Caracas", "Valencia"],
+    Canada: ["Ottowa", "Montreal", "Toronto"],
+  };
+
+  // Function to update the options in the second dropdown based on the selection in the first dropdown
+  function updateCityDropdown() {
+    const selectedOption = countryDropdown.value;
+    const options = optionsBySelection[selectedOption];
+
+    //if found
+    if (options != undefined){
+      populateDropdown(options, cityDropdown);
+    }
+  }
+
+  // Function to update the options in the first dropdown based on the selection in the second dropdown
+  function updateCountrydDropdown() {
+    const selectedOption = cityDropdown.value;
+    for (const key in optionsBySelection) {
+      if (optionsBySelection[key].includes(selectedOption)) {
+        countryDropdown.value = key;
+        break;
+      }
+    }
+  }
+
+  // Function to populate a dropdown with the provided options
+  function populateDropdown(options, dropdown) {
+    dropdown.innerHTML = ""; // Clear existing options
+
+    const optionElement = document.createElement("option");
+    optionElement.selected = true;
+    optionElement.disabled.value = "";
+    optionElement.textContent = "Please select an option";
+    dropdown.appendChild(optionElement);
+
+    // Add new options to the dropdown
+    for (const option of options) {
+      const optionElement = document.createElement("option");
+      optionElement.textContent = option;
+      dropdown.appendChild(optionElement);
+    }
+  }
+
+  // Add event listeners to both dropdowns to trigger the updates
+  cityDropdown.addEventListener("change", updateCountrydDropdown);
+  countryDropdown.addEventListener("change", updateCityDropdown);
+
+  // Initial update when the page loads
+  updateCityDropdown();
+}
+
+//================================================================
